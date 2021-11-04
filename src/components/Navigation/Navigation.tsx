d1cce6542e7ba4b8b801e532ac99e9da05c90670
Yunus <yunusgogce@gmail.com>
2021-11-04 16:15:34 +0100

    added hamburger menu for mobile

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,30 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Navigation.css";
 import { Navbar, Nav, Container } from "react-bootstrap";
 import AnchorLink from "react-anchor-link-smooth-scroll";
 
 function Navigation() {
+  const [expanded, setExpanded] = useState(false);
+
+  function toggleMenu(): void {
+    setExpanded(!expanded);
+  }
+
   return (
     <>
-      <Navbar variant="dark" className="navbar-fixed">
+      <Navbar
+        collapseOnSelect={true}
+        expanded={expanded}
+        fixed="top"
+        variant="dark"
+        expand="md"
+        className="navbar-bg"
+      >
         <Container>
-          <AnchorLink href="#intro">
+          <AnchorLink href="#intro" onClick={toggleMenu}>
             <Navbar.Brand>{"<Portfolio />"}</Navbar.Brand>
           </AnchorLink>
-          <Nav className="justify-content-end">
-            <AnchorLink href="#aboutme">
-              <Nav.Item>About me</Nav.Item>
-            </AnchorLink>
-            <AnchorLink href="#projects">
-              <Nav.Item>Projects</Nav.Item>
-            </AnchorLink>
-            <AnchorLink href="#work">
-              <Nav.Item>Work</Nav.Item>
-            </AnchorLink>
-            <AnchorLink href="#contact">
-              <Nav.Item>Contact</Nav.Item>
-            </AnchorLink>
-          </Nav>
+          <Navbar.Toggle
+            onClick={toggleMenu}
+            aria-controls="responsive-navbar-nav"
+          />
+          <Navbar.Collapse
+            id="responsive-navbar-nav"
+            className="justify-content-end"
+          >
+            <Nav>
+              <AnchorLink href="#aboutme" onClick={toggleMenu}>
+                <Nav.Item>About me</Nav.Item>
+              </AnchorLink>
+              <AnchorLink href="#projects" onClick={toggleMenu}>
+                <Nav.Item>Projects</Nav.Item>
+              </AnchorLink>
+              <AnchorLink href="#work" onClick={toggleMenu}>
+                <Nav.Item>Work</Nav.Item>
+              </AnchorLink>
+              <AnchorLink href="#contact" onClick={toggleMenu}>
+                <Nav.Item>Contact</Nav.Item>
+              </AnchorLink>
+            </Nav>
+          </Navbar.Collapse>
         </Container>
       </Navbar>
     </>
