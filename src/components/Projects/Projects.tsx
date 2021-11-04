4e412c92038579d084ee05b9dc6f88074cfb54cb
Yunus <yunusgogce@gmail.com>
2021-11-04 16:51:10 +0100

    animation for projects

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import "./Projects.css";
 import IProject from "./IProject";
 import projectsData from "./projects.json";
+const Fade = require("react-reveal/Fade");
 
 function Projects() {
   const [projects] = useState<Array<IProject>>(projectsData.projects);
@@ -10,15 +11,17 @@ function Projects() {
     <div className="projects text-light" id="projects">
       <div className="projects-bg">
         <h2 className="h1 mb-5 hr">{"<Projects />"}</h2>
-        {projects.map((project, i) => {
-          return (
-            <div key={i} className="project-card p-4 mb-4">
-              <p className="h4">{project.title}</p>
-              <hr />
-              <p>{project.description}</p>
-            </div>
-          );
-        })}
+        <Fade>
+          {projects.map((project, i) => {
+            return (
+              <div key={i} className="project-card p-4 mb-4">
+                <p className="h4">{project.title}</p>
+                <hr />
+                <p>{project.description}</p>
+              </div>
+            );
+          })}
+        </Fade>
       </div>
     </div>
   );
